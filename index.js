06d5f0abf8a4bea3c833252ff3493a4930081822
AnhHoangNgo <ngoanh1292000@gmail.com>
2019-09-21 04:20:34 +0700

    commit

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ controller.loatScript('https://code.jquery.com/jquery-3.4.1.min.js')
           $('header.header').addClass('activex');
       });
       controller.getDataAPI('./history.json');
-      controller.getDataAPI('.imageLamborghini.json');
+      controller.getDataAPI('./imageLamborghini.json');
 
 });
 });
