214b8d75200b0677ec443669c542a9676c7ae23d
AnhHoangNgo <ngoanh1292000@gmail.com>
2019-09-24 23:20:33 +0700

    commit

--- components.js
+++ components.js
@@ -25,106 +25,64 @@ components.header =`
 </div>
 `
 components.home=`
-        <header class="headerPage">
-        <h1 class="title">LAMBORGHINI</h1>
-        <div class="slideShow slideShow1"><img class="imageShow" src="https://cdn.shopify.com/s/files/1/0043/8471/8938/products/155613364003514899.jpg?v=1565119707" alt=""></div>
-        <div class="slideShow slideShow1"><img class="imageShow" src="https://cdn.shopify.com/s/files/1/0043/8471/8938/products/155613364003514899.jpg?v=1565119707" alt=""></div>
-        <div class="slideShow slideShow2"><img class="imageShow" src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/homepage/EVO%20SPYDER%20MOBILE.jpg" alt=""></div>
-        <div class="slideShow slideShow3"><img class="imageShow" src="https://upload.wikimedia.org/wikipedia/commons/0/07/Lamborghini_Veneno.jpg" alt=""></div>
-        <div class="dot-container">
-           <span  class="dot"></span>
-           <span  class="dot"></span>
-           <span  class="dot"></span>
-           <span  class="dot"></span>
-        </div>
-        <a href="#page-2"><span></span></a>
-   </header>
-   <main class="mainPage">
-   <section><div class="Asus">
-   <h1>Harucan</h1>
-   <p>From our past, we've learned perfection. This is how the first model of the Lamborghini Huracán was born. 
-     More performance, more control, more innovation. The Huracán is equipped with a powerful V10 engine and the 
-     latest technologies to perform at its absolute best. Engineered to guarantee the best performance and control 
-     under any driving condition, it's easy to take to the limit, not to mention extremely fun, thanks to technology 
-     like the Lamborghini Doppia Frizione (LDF) dual clutch gearbox and the electronically-controlled four-wheel drive.
-      Enjoy the best driving experience, ever. Discover all the Lamborghini Huracán models. </p>
-  <div class="container">
-    <div class="mySlides">
-      <div class="numberText">1/3</div>
-      <img src="tải xuống (1).jpg" alt="asus car" style ="width:100%">
-    </div>
-    <div class="mySlides">
-       <div class="numberText">2/3</div>
-       <img src="tải xuống (2).jpg" alt="asus car" style ="width:100%">
-     </div>
-     <div class="slides">
-         <div class="numberText">3/3</div>
-         <img src="tải xuống (3).jpg" alt="asus car" style ="width:100%">
-       </div>
-       <a class="prev" onclick="plusSlides(-1)">❮</a>
-       <a class="next" onclick="plusSlides(1)">❯</a>
-       <div class="row">
-           <div class="column">
-             <img class="demo cursor" src="tải xuống (1).jpg" style="width:100%" onclick="currentSlide(1)" alt="The Woods">
-           </div>
-           <div class="column">
-             <img class="demo cursor" src="tải xuống (2).jpg" style="width:100%" onclick="currentSlide(2)" alt="Cinque Terre">
-           </div>
-           <div class="column">
-             <img class="demo cursor" src="tải xuống (3).jpg" style="width:100%" onclick="currentSlide(3)" alt="Mountains and fjords">
-           </div>
-  </div>
- </div>
-</section>
-<section>
-   <div class="Asus">
-     <h1>Aventador</h1>
-     <p>The Aventador has been created to anticipate the future, as demonstrated by the use of innovative technology, including a V12 engine and the extensive use of carbon fiber.
-         The authentic design masterpieces together stark dynamism with aggression to produce a cutting edge carbon fiber monocoque. The interior of the Aventador combines
-          high-level technology and luxury equipment with premium-quality materials, skilfully crafted with the expertise characteristic of the finest Italian traditions.
-           A supercar family that has already become a legend in its own right. Discover technical specifications, dimensions, performance, and the detailed features of
-            all the new Lamborghini Aventador models.</p>
-    <div class="container">
-      <div class="mySlides">
-        <div class="numberText">1/3</div>
-        <img src="tải xuống (1).jpg" alt="asus car" style ="width:100%">
-      </div>
-      <div class="mySlides">
-         <div class="numberText">2/3</div>
-         <img src="tải xuống (2).jpg" alt="asus car" style ="width:100%">
-       </div>
-       <div class="slides">
-           <div class="numberText">3/3</div>
-           <img src="tải xuống (3).jpg" alt="asus car" style ="width:100%">
-         </div>
-         <a class="prev" onclick="plusSlides(-1)">❮</a>
-         <a class="next" onclick="plusSlides(1)">❯</a>
-         <div class="row">
-             <div class="column">
-               <img class="demo cursor" src="tải xuống (1).jpg" style="width:100%" onclick="currentSlide(1)" alt="The Woods">
-             </div>
-             <div class="column">
-               <img class="demo cursor" src="tải xuống (2).jpg" style="width:100%" onclick="currentSlide(2)" alt="Cinque Terre">
-             </div>
-             <div class="column">
-               <img class="demo cursor" src="tải xuống (3).jpg" style="width:100%" onclick="currentSlide(3)" alt="Mountains and fjords">
-             </div>
-    </div>
-   </div>
- </section></section>
-   </main>
+
+        
 `;
 components.model=`
-<section id="page-1" class="page header">
-          <h1>Model</h1>
+<header class="headerPage">
+          <h1 class="title">LAMBORGHINI </br> MODEL</h1>
+          <div class="slideShow slideShow1"><img class="imageShow" src="lamborghini-forsennato-concept.jpg" alt=""></div>
+          <div class="slideShow slideShow1"><img class="imageShow" src="lamborghini-sian-roadster-rendering.jpg" alt=""></div>
+          <div class="slideShow slideShow2"><img class="imageShow" src="D1ZJbigX0AAkTL9.jpg" alt=""></div>
+          <div class="slideShow slideShow3"><img class="imageShow" src="lamborghini-aventador-svj-roadster-i.jpg" alt=""></div>
+          <div class="dot-container">
+             <span  class="dot"></span>
+             <span  class="dot"></span>
+             <span  class="dot"></span>
+             <span  class="dot"></span>
+          </div>
           <a href="#page-2"><span></span></a>
-        </section>
-        <section id="page-2" class="page">
-        </section>
-        <section id="page-3" class="page">
-        </section>
-        <section id="page-4" class="page">
-        </section>
+     </header>
+     <main class="mainPage">
+     <div class="pageNow"> <a href="?page=home">Home</a> / <a href="?page=model">Models</a></div>
+     <div class="firstMainPageModel">
+     <img class="logo" src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/gateway-models/urus/Urus-logo-2.png">
+     <p>From our past, we've learned perfection. This is how the first model of the Lamborghini Huracán was born.
+      More performance, more control, more innovation. The Huracán is equipped with a powerful V10 engine and the 
+      latest technologies to perform at its absolute best. Engineered to guarantee the best performance and control
+       under any driving condition, it's easy to take to the limit, not to mention extremely fun, thanks to technology
+        like the Lamborghini Doppia Frizione (LDF) dual clutch gearbox and the electronically-controlled four-wheel drive.
+         Enjoy the best driving experience, ever. Discover all the Lamborghini Huracán models. </p>
+      <div class="imageAboutCar">
+      <img style=" width:100%" src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/gateway-models/urus/car-urus.png">
+      </div>
+     </div>
+     <div class="secondMainPageModel">
+     <img class="logo" src="https://www.lamborghini.com/sites/it-en/files/models_gateway/logo-huracan.png">
+     <p>From our past, we've learned perfection. This is how the first model of the Lamborghini Huracán was born. 
+     More performance, more control, more innovation. The Huracán is equipped with a powerful V10 engine and the 
+     latest technologies to perform at its absolute best. Engineered to guarantee the best performance and control
+      under any driving condition, it's easy to take to the limit, not to mention extremely fun, thanks to technology
+       like the Lamborghini Doppia Frizione (LDF) dual clutch gearbox and the electronically-controlled four-wheel drive.
+        Enjoy the best driving experience, ever. Discover all the Lamborghini Huracán models. </p>
+        <div class="imageAboutCar">
+        <img style=" width:100%" src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/model/huracan/evo-slider/model/Huracan%20Spyder_scontorno.png">
+        </div>
+       </div>
+     <div class=thirdMainPageModel">
+     <img class="logo" src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/gateway-family/aventador/logo-aventador.png">
+     <p>The Aventador has been created to anticipate the future, as demonstrated by the use of innovative technology,
+      including a V12 engine and the extensive use of carbon fiber.
+     The authentic design masterpieces together stark dynamism with aggression to produce a cutting edge carbon fiber
+      monocoque. The interior of the Aventador combines high-level technology and luxury equipment with premium-quality 
+      materials, skilfully crafted with the expertise characteristic of the finest Italian traditions. A supercar family
+       that has already become a legend in its own right. Discover technical specifications, dimensions, performance, and 
+       the detailed features of all the new Lamborghini Aventador models.</p>
+       <div class="imageAboutCar">
+       <img style=" width:100%" src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/model/aventador/aventador-svj-roadster/car/SVJ_Roadster_gateway%20modelli.png">
+       </div>
+     </div>
+     </main>
 `;
 components.brand=`
 <section id="page-1" class="page header">
@@ -164,16 +122,15 @@ components.new=`
         </section>
 `
 components.album=`
-<section id="page-1" class="page header">
-          <h1>Album</h1>
-          <a href="#page-2"><span></span></a>
-        </section>
-        <section id="page-2" class="page">
-        </section>
-        <section id="page-3" class="page">
-        </section>
-        <section id="page-4" class="page">
-        </section>
+<header class="headerPage">
+<h1 class="title">LAMBORGHINI  </br> ALBUM</h1>
+<div class="slideShow slideShow1"><img class="imageShow" src="aventador_mk2_render.jpg" alt=""></div>
+<a href="#page-2"><span></span></a>
+</header>
+<main class="mainPage">
+<h1 style = "color:black">Masonry Gallery</h1>
+<div class="gallery" id="gallery">
+</div></main>
 `
 components.contact=`
 <section id="page-1" class="page header">
--- view.js
+++ view.js
@@ -3,23 +3,24 @@ view.setActiveScreen = (screenName) => {
     switch(screenName){
         case'home':
         document.getElementById('page-container').innerHTML = components.home;
+        break;
+        case'model':
+        document.getElementById('page-container').innerHTML = components.model;
         const slideShows = document.getElementsByClassName('slideShow');
         const dots = document.getElementsByClassName('dot')
         var slideShowIndex = 0;
         slideShowImage();
         function slideShowImage(){
+        slideShowIndex++;
+        if(slideShowIndex>slideShows.length){slideShowIndex=1};
         for(let i=0; i<slideShows.length; i++){
         slideShows[i].style.display ="none";
-        dots[i].classList.remove('close');
+        dots[i].classList.remove('active');
     };
-    slideShowIndex++;
-    if(slideShowIndex>slideShows.length){slideShowIndex=1};
     slideShows[slideShowIndex-1].style.display="block";
-    dots[slideShowIndex -1].classList.add('close');
+    dots[slideShowIndex -1].classList.add('active');
     setTimeout(slideShowImage,2000);
-}       break;
-        case'model':
-        document.getElementById('page-container').innerHTML = components.model;
+}
         break;
         case'brand':
         document.getElementById('page-container').innerHTML = components.brand;
