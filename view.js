9d14e7bc4f238ae6586149d0039ad65d14c3c953
AnhHoangNgo <ngoanh1292000@gmail.com>
2019-09-28 00:20:32 +0700

    commit

--- view.js
+++ view.js
@@ -36,7 +36,7 @@ view.setActiveScreen = (screenName) => {
                 const newsContainer = document.querySelector(".newsContainer");
                 newsContainer.innerHTML += `
                 <div class="new">
-                 a href="?page=news&id=${elem.id}"> 
+                 <a href="?page=news&id=${elem.id}"> 
                  <img style = "width:100%" src="${elem.img.url} alt=""></a>
                  <div class="seeNew"> <h4>${elem.date}</h4> <a href="?page=news&id=${elem.id}">DISCOVER MORE</a></div>
                  <h3 class="title">${elem.title}</h3>
